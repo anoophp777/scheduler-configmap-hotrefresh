8f8ec74c614b759264fa1351c4a326d7606a9d1c
Anoop Hallimala <anoop.hallimala@lowes.com>
2020-05-22 20:20:26 +0530

    Adding code to create namespace automatically

--- build-deploy-to-kubernetes.sh
+++ build-deploy-to-kubernetes.sh
@@ -4,7 +4,7 @@ mvn clean package -DskipTests
 docker rmi anoophallimala/configmaps:v1
 docker build -t anoophallimala/configmaps:v1 .
 kubectl delete -f kube-configmap.yml
+kubectl create -f create-some-ns.json
 kubectl create -f kube-configmap.yml
 kubectl create -f deployment-with-configmaps.yml
 kubectl get pods
-#kubectl expose deployment springboot-configmap-k8s --type="NodePort" --port 6060
